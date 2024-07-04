d068008293c217a5b4b6800c8a654caac468a08f
Mychnlan <milanhn518@gmail.com>
2024-07-04 21:05:48 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+// import daisyui from "daisyui";
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+          // Tambahkan warna lain sesuai kebutuhan
+        },
+      },
+      // Atau bisa menggunakan tema default DaisyUI tanpa tema gelap
+      "dark",
+    ],
+  },
+};
